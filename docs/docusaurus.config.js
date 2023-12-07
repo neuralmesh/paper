b7d6784d02b56ea237cd2522f6b64928724edb02
m-c-frank <martin7.frank7@gmail.com>
2023-12-07 07:14:44 +0100

    squash merge docs

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,75 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'paper',
+  tagline: 'the open source research paper',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://paper.apimesh.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'neuralmesh', // Usually your GitHub org/user name.
+  projectName: 'paper', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        blog: {
+          path: './blog', // Path to the blog directory outside of the Docusaurus root
+          showReadingTime: true,
+          // Optionally, specify the edit URL
+          editUrl: 'https://github.com/neuralmesh/paper/edit/main/blog/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      },
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      title: 'paper',
+      logo: {
+        alt: 'Your Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        { to: '/blog', label: 'Blog', position: 'left' },
+        // Add other navbar items if needed
+      ],
+    },
+    footer: {
+      style: 'dark',
+      // Define footer links and copyright as needed
+    },
+    // Define your prism theme settings if needed
+  },
+};
+
+export default config;
+
